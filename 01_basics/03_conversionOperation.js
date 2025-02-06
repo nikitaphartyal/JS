c34d371fd5e56602b4f6dc888106ed1f1106876f
Nikita Phartyal <133484285+nikitaphartyal@users.noreply.github.com>
2025-02-06 07:56:01 +0000

    conversion operations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let age = "21niki" // "nikita",21,null,undefined,true/false
+
+console.log(typeof age);
+console.log(typeof (age));
+
+let valueInNumber = Number(age);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// true => 1; False => 0
+// "21niki" => NaN
+
+let isLoggedIn = "Nikita"
+let booleanisLoggedIn = Boolean(isLoggedIn);
+console.log(booleanisLoggedIn);
+
+// 1 =>true; 0 =>false
+// "" =>false
+// "Nikita" => true
+
+let someNumber = 21
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
