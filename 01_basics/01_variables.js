2716a817da352b08e183b0aaf799afffc16d9658
Nikita Phartyal <133484285+nikitaphartyal@users.noreply.github.com>
2025-02-06 06:33:51 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 1796566
+let accountEmail = "nikita@gmail.com"
+var accountPassword = "7894"
+accountCity = "Chennai";
+
+
+accountEmail= "nkfnl@gmail.com";
+accountPassword="4568";
+accountCity = "Mumbai";
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword]);
+
+
