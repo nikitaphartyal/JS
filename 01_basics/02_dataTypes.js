91e90dd696da8edabdc5d7db4702991cea0d2704
Nikita Phartyal <133484285+nikitaphartyal@users.noreply.github.com>
2025-02-06 11:00:50 +0000

    message

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict"   // Treat all JS code as newer version
+
+console.log(3
+    +
+     3) //code readability should be high
+
+
+console.log("Nikita");
+
+let name = "Nikita"
+let age = 21;
+let isLoggedIn=false;
+
+
+console.log(typeof null); //object
--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,26 +1,55 @@
-let age = "21niki" // "nikita",21,null,undefined,true/false
+// let age = "21niki" // "nikita",21,null,undefined,true/false
 
-console.log(typeof age);
-console.log(typeof (age));
+// console.log(typeof age);
+// console.log(typeof (age));
 
-let valueInNumber = Number(age);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(age);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
-// true => 1; False => 0
-// "21niki" => NaN
+// // true => 1; False => 0
+// // "21niki" => NaN
 
-let isLoggedIn = "Nikita"
-let booleanisLoggedIn = Boolean(isLoggedIn);
-console.log(booleanisLoggedIn);
+// let isLoggedIn = "Nikita"
+// let booleanisLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanisLoggedIn);
 
-// 1 =>true; 0 =>false
-// "" =>false
-// "Nikita" => true
+// // 1 =>true; 0 =>false
+// // "" =>false
+// // "Nikita" => true
 
-let someNumber = 21
+// let someNumber = 21
 
-let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
+
+// *********************Operations*********************
+
+let str1= "Hello "
+let str2 = "Nikita"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log((3+4)*5%3);
+
+console.log(+true);
+console.log(+"");
+
+
+let num1,num2,num3
+num1=num2=num3=5+6
+
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter);
+
+let gameCounterr=100
+gameCounterr--;
+console.log(gameCounterr);
